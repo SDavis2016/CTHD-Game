add5214e8001e1c71ca34f0c611ad4b58305d361
Guest User <coder@mainlab28.local>
2016-04-25 11:04:27 -0700

    s

---
+++ CTHG/Lives.swift
@@ -0,0 +1,24 @@
+//
+//  Lives.swift
+//  CTHG
+//
+//  Created by Coder on 4/25/16.
+//  Copyright © 2016 Epic Gaming. All rights reserved.
+//
+
+import Foundation
+// When timer runs out or wrong answer lives decrease by one
+
+var timerIndicator = 15
+var answer = true
+
+func countingOfLives(amt: Int) -> Int {
+    var lives = amt
+    if timerIndicator == 0 {
+        lives -= lives
+    }
+    if answer == false {
+        lives -= lives
+    }
+    return lives
+}
