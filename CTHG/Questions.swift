2fd36e3529f85b6eba8e98f45d8c5258015b75d3
Guest User <coder@mainlab28.local>
2016-05-12 11:00:29 -0700

    s

--- CTHG/Questions.swift
+++ CTHG/Questions.swift
@@ -10,13 +10,15 @@ import Foundation
 
 var questionList :[String] = [
     "Welcome to the True or False Challenge! Press true to begin!", "Jhon Keats is a Romantic poet", "Velcro in the inside of atronaughts helmets is to hold their gum", "Lightning never strikes in the same place twice.", "If you cry in space the tears just stick to your face.", "If you cut an earthworm in half, both halves can regrow their body.", "Humans can distinguish between over a trillion different smells.", "Adults have fewer bones than babies do.", "Napoleon Bonaparte was extremely short.", "Goldfish only have a memory of three seconds.", "There are more cells of bacteria in your body than there are human cells.", "Is the cake a lie","Your fingernails and hair keep growing after you die.",
+    
     "You Reached Level 2! Press true to continue.", "Birds are dinosaurs", "It costs the U.S. Mint more to make pennies and nickels than the coins are actually worth.", "Water spirals down the plughole in opposite directions in the northern and southern hemispheres", "Water spirals down the plughole in opposite directions in the northern and southern hemispheres",
     
 ]
 
 var answerList :[Int: Bool] = [
-    0:true, 1:true, 2:false, 3:false, 4:true, 5:false, 6:true, 7:true, 8:false, 9:false, 10:true, 11:true,
-    12:false, 13:true, 14:true, 15:false
+    0:true, 1:true, 2:false, 3:false, 4:true, 5:false, 6:true, 7:true, 8:false, 9:false, 10:true, 11:true, 12:false,
+    
+    13:true,14:true, 15:false
 ]
 
 
