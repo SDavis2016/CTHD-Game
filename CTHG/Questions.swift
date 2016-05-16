3c070dd726cbe7e0df6d64f64bcb971f9bcda536
Guest User <coder@mainlab29.local>
2016-05-16 10:35:21 -0700

    fix the problem

--- CTHG/Questions.swift
+++ CTHG/Questions.swift
@@ -13,7 +13,7 @@ var questionList :[String] = [
     
     "You Reached Level 2! Press true to continue.", "Birds are dinosaurs", "It costs the U.S. Mint more to make pennies and nickels than the coins are actually worth.", "Water spirals down the plughole in opposite directions in the northern and southern hemispheres", "Buzz Aldrin was the first man to urinate on the moon", "Twinkies have an infinite shelf life", "Humans can’t breathe and swallow at the same time", "The popular image of Santa Claus – chubby, bearded, in red and white clothes – was invented by Coca-Cola for an ad campaign.", "The top of the Eiffel Tower leans away from the sun", "Drinking alcohol kills brain cells", "The owner of the company that makes Segways died after accidentally driving his Segway off a cliff.", "As far as has ever been reported, no-one has ever seen an ostrich bury its head in the sand.", "Approximately one quarter of human bones are in the feet.",
     
-    "You Reached Level 3! Press true to continue.", "Popeye’s nephew’s were called Peepeye, Poopeye, Pipeye and Pupeye.", "In ancient Rome, a special room called a vomitorium was available for diners to purge food in during meals.", "The average person will shed 10 pounds of skin during their lifetime.", "Sneezes regularly exceed 100 m.p.h.", "A slug’s blood is green.", "The Great Wall Of China is visible from the moon.", "Virtually all Las Vegas gambling casinos ensure that they have no clocks.", "The total surface area of two human lungs have a surface area of approximately 70 square metres.", "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", "Valentine’s day is banned in in Saudi Arabia.", "In China you cannot use Facebook or YouTube, but you can use Twitter.", "In Barcelona you are free to wear a bikini, swimming trunks or go bare-chested on the streets."
+    "You Reached Level 3! Press true to continue.", "Popeye’s nephew’s were called Peepeye, Poopeye, Pipeye and Pupeye.", "In ancient Rome, a special room called a vomitorium was available for diners to purge food in during meals.", "The average person will shed 10 pounds of skin during their lifetime.", "Sneezes regularly exceed 100 m.p.h.", "A slug’s blood is green.", "The Great Wall Of China is visible from the moon.", "Virtually all Las Vegas gambling casinos ensure that they have no clocks.", "The total surface area of two human lungs have a surface area of approximately 70 square metres.", "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", "Valentine’s day is banned in in Saudi Arabia.", "In China you cannot use Facebook or YouTube, but you can use Twitter.", "In Barcelona you are free to wear a bikini, swimming trunks or go bare-chested on the streets.",
     
     
 ]
@@ -23,7 +23,7 @@ var answerList :[Int: Bool] = [
     
     13:true, 14:true, 15:false, 16:false, 17:true, 18:false, 19:true, 20:false, 21:true, 22:false, 23:true, 24:true, 25:true,
 
-    26:true, 27:true, 28:false, 29:false, 30:true, 31:true, 32:false, 33:true, 34:true, 35:true, 36:true, 36:false, 37:false,
+    26:true, 27:true, 28:false, 29:false, 30:true, 31:true, 32:false, 33:true, 34:true, 35:true, 36:true, 37:false, 38:false,
 ]
 
 
